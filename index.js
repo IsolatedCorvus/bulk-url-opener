ccad95e60af0443c4d6800eb563b732aa718d37b
Iso <76796780+IsolatedCorvus@users.noreply.github.com>
2025-09-25 14:36:38 -0700

    button active/inactive state, fixed bugs, editable link items

--- index.js
+++ index.js
@@ -7,66 +7,63 @@ const urlRegEx = /https?:\/\/[^\s]*/ig;
 const btnOptions = document.querySelector(".btn-container");
 
 // Events
-const btn = document.getElementById("btn-submit");
-btn.addEventListener("click", handleUserInput);
+const submitBtn = document.getElementById("btn-submit");
+submitBtn.addEventListener("click", handleUserInput);
 
 document.getElementById("open-all").addEventListener("click", openAll);
-document.getElementById("clear-all").addEventListener("click", clear);
+document.getElementById("clear-all").addEventListener("click", () => clear(1));
 
-
-// TODO: button only active when VALID link is available.
-// Buttons inactive while text-area is empty
-/*
-btn.setAttribute("disabled", true);
-
-textArea.addEventListener("input", () => {
-    if(textArea.value.length){
-        console.log("HERE");
-        btn.removeAttribute("disabled");
-    }
-});
-*/
-
-
+// Links Array
 let importedLinks = [];
 
+
+//TODO: debounce event
+// Button is only active when text-area has input
+let submitBtnTimer;
+submitBtn.setAttribute("disabled", true);
+textArea.addEventListener("input", () => {
+    clearTimeout(submitBtnTimer);
+    submitBtnTimer = setTimeout(() => {
+        console.log("HERE");
+        textArea.value.length ? submitBtn.removeAttribute("disabled") : submitBtn.setAttribute("disabled", true);
+    }, 100);
+});
+
+// TODO: able to selectively remove links from link-list
+// TODO: able to edit specific link list item
+
 function handleUserInput() {
-    if (!textArea.value) {
-        // display popup then hide
+    const httpRegEx = /https?/ig;
+    let userInput = textArea.value;
+
+    // empty input or no urls, then display popup -> hide
+    if (!httpRegEx.test(userInput)) {
         const popUpWarning = document.querySelector(".warning-pop-up");
         popUpWarning.classList.remove("hide");
         setTimeout(() => popUpWarning.classList.add("hide"), 2000);
+        return;
     }
-    // window.open(textArea.value);
-
-    let userInput = textArea.value;
-
-    // clear previous input
-    importedLinks = [];
-    linkList.innerHTML = "";
-    btnOptions.classList.remove("hide");
-    btn.setAttribute("disabled", false);
 
     // for concatenated links with no clear delimitors
     //* .matchAll() returns an iterator, spread operator to place into array []
-    const httpRegEx = /https?/ig;
     if([...userInput.matchAll(httpRegEx)].length > 1) {
         userInput = linkSeperator(userInput);
     }
 
+    // clear previous input
+    clear(0);
+
     // .exec() returns null if match fails, else returns array of matches
     let arrMatch;
     while ((arrMatch = urlRegEx.exec(userInput)) !== null) {
-        // console.log(arrMatch[0]);
-
         importedLinks.push(arrMatch[0]);
         console.log(importedLinks);
 
         linkList.innerHTML += `
             <article class="link-list-item">
                 <p>${arrMatch[0]}</p>
-            </article>
-        `;
+                <span class="remove-link-item" title="Remove">X</span>
+            </article>`;
     }
     linkUpdator();
 }
@@ -76,8 +73,19 @@ function linkUpdator() {
     const links = document.querySelectorAll(".link-list-item");
     links.forEach((link, index) => {
         link.addEventListener("click", () => {
-            // console.log(importedLinks[index]);
             window.open(importedLinks[index]);
+            // console.log(importedLinks[index]);
+        });
+    });
+
+    // removeable links
+    const removeLinks = document.querySelectorAll(".remove-link-item");
+    removeLinks.forEach((link, index) => {
+        link.addEventListener("click", (e) => {
+            link.parentElement.remove();     // remove the parent element (link-list-item)
+            importedLinks.splice(index, 1);  // remove from the array
+            console.log(importedLinks);
+            e.stopPropagation();  // stops from opening the link
         });
     });
 }
@@ -93,16 +101,17 @@ function linkSeperator(userInput){
 
 function openAll() {
     for (const link of importedLinks) {
-        window.open(link);
+        window.open(link);  // window.open(url)
     }
 }
 
-function clear() {
+function clear(mode = 0) {   // mode 0 - clear previous, mode 1 - clear all
     importedLinks = [];
-    linkList.innerHTML = "<h2>URLs:</h2>";
+    linkList.innerHTML = "";
     const btnOptions = document.querySelector(".btn-container");
-    btnOptions.classList.add("hide");
-    // btn.setAttribute("disabled", true);
+    mode 
+        ? btnOptions.classList.add("hide")
+        : btnOptions.classList.remove("hide");
 }
 
 //TODO: ???
@@ -110,8 +119,14 @@ function inputSanitationandValidation(){
 
 }
 
-// https://youtu.be/T1EJZsaqevU
-// https://youtu.be/buvWBOZTfdc
-// https://www.youtube.com/watch?v=XJC5WB2Bwrc
 
-// https://youtu.be/T1EJZsaqevUhttps://youtu.be/buvWBOZTfdchttps://www.youtube.com/watch?v=XJC5WB2Bwrc
+/*
+https://youtu.be/T1EJZsaqevU
+https://youtu.be/buvWBOZTfdc
+https://www.youtube.com/watch?v=XJC5WB2Bwrc
+
+https://youtu.be/T1EJZsaqevUhttps://youtu.be/buvWBOZTfdchttps://www.youtube.com/watch?v=XJC5WB2Bwrc
+
+https://youtu.be/T1EJZsaqevU https://youtu.be/buvWBOZTfdc https://www.youtube.com/watch?v=XJC5WB2Bwrc
+
+*/
