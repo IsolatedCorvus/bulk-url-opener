e4aa967b5c475e0df6b1a8c9f4682c65b3c44693
Iso <76796780+IsolatedCorvus@users.noreply.github.com>
2025-09-30 15:54:51 -0700

    editable links, dynamic popups, restructuring

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bulk-url-opener
 Bulk URL Opener written in JavaScript
+---
+**Features**
+- Singular and Bulk URL opening
+- RegEx for pattern matching of strings containing HTTP/s protocols
+- Editable Lists (Delete, Edit)
+---
+**Why?**
+- Easier to open multiple links 4 gooning.
--- index.js
+++ index.js
@@ -16,6 +16,9 @@ document.getElementById("clear-all").addEventListener("click", () => clear(1));
 // Links Array
 let importedLinks = [];
 
+// Event Flag
+let enableClickEvent = true;
+
 
 //TODO: debounce event
 // Button is only active when text-area has input
@@ -38,15 +41,13 @@ function handleUserInput() {
 
     // empty input or no urls, then display popup -> hide
     if (!httpRegEx.test(userInput)) {
-        const popUpWarning = document.querySelector(".warning-pop-up");
-        popUpWarning.classList.remove("hide");
-        setTimeout(() => popUpWarning.classList.add("hide"), 2000);
+        displayPopUp("Please input URL(s)");
         return;
     }
 
     // for concatenated links with no clear delimitors
     //* .matchAll() returns an iterator, spread operator to place into array []
-    if([...userInput.matchAll(httpRegEx)].length > 1) {
+    if ([...userInput.matchAll(httpRegEx)].length > 1) {
         userInput = linkSeperator(userInput);
     }
 
@@ -59,11 +60,13 @@ function handleUserInput() {
         importedLinks.push(arrMatch[0]);
         console.log(importedLinks);
 
-        linkList.innerHTML += `
-            <article class="link-list-item">
-                <p>${arrMatch[0]}</p>
+        linkList.innerHTML +=
+            ` <article class="link-list-item">
+                <span class="edit-link-item" title="Edit">E</span>
+                <input class="list-editable-text display-none" type="text" name="" id="" required>
+                <p class="list-text">${arrMatch[0]}</p>
                 <span class="remove-link-item" title="Remove">X</span>
-            </article>`;
+            </article>`
     }
     linkUpdator();
 }
@@ -73,14 +76,15 @@ function linkUpdator() {
     const links = document.querySelectorAll(".link-list-item");
     links.forEach((link, index) => {
         link.addEventListener("click", () => {
-            window.open(importedLinks[index]);
+            // opens only if flag is true
+            if(enableClickEvent) window.open(importedLinks[index]);
             // console.log(importedLinks[index]);
         });
     });
 
     // removeable links
-    const removeLinks = document.querySelectorAll(".remove-link-item");
-    removeLinks.forEach((link, index) => {
+    const removeLinkBtns = document.querySelectorAll(".remove-link-item");
+    removeLinkBtns.forEach((link, index) => {
         link.addEventListener("click", (e) => {
             link.parentElement.remove();     // remove the parent element (link-list-item)
             importedLinks.splice(index, 1);  // remove from the array
@@ -88,9 +92,41 @@ function linkUpdator() {
             e.stopPropagation();  // stops from opening the link
         });
     });
+
+    // editable links
+    const editLinkBtns = document.querySelectorAll(".edit-link-item");
+    editLinkBtns.forEach((link, index) => {
+        link.addEventListener("click", e => {
+            e.stopPropagation();  // stops from opening the link
+
+            let newURL = link.parentNode.querySelector(".list-editable-text");
+            let currentURL = link.parentNode.querySelector(".list-text");
+
+            // toggles between current url or editable text field
+            if (newURL.classList.contains("display-none")) {
+                newURL.classList.remove("display-none");
+                currentURL.classList.add("display-none");
+                enableClickEvent = false;  // temporarily disables link opening
+            } else {
+                newURL.classList.add("display-none");
+                currentURL.classList.remove("display-none");
+                enableClickEvent = true;  //re-enables
+
+                // updates array of links to new inputs
+                importedLinks.splice(index, 1, currentURL.innerHTML);
+                console.log("BRUH", importedLinks);
+            }
+
+            newURL.addEventListener("input", e => {
+                urlRegEx.test(e.target.value)
+                    ? currentURL.innerHTML = e.target.value
+                    : displayPopUp("Needs to be Valid URL");
+            });
+        });
+    });
 }
 
-function linkSeperator(userInput){
+function linkSeperator(userInput) {
     // splits string
     let urlArr = userInput.split("http");
     urlArr.shift(); // removing first empty element, when delimiter is the beginning of string
@@ -109,16 +145,23 @@ function clear(mode = 0) {   // mode 0 - clear previous, mode 1 - clear all
     importedLinks = [];
     linkList.innerHTML = "";
     const btnOptions = document.querySelector(".btn-container");
-    mode 
+    mode
         ? btnOptions.classList.add("hide")
         : btnOptions.classList.remove("hide");
 }
 
-//TODO: ???
-function inputSanitationandValidation(){
-
+// Dynamic Popup
+function displayPopUp(msg = ""){
+    const popUpWarning = document.querySelector(".warning-pop-up");
+    popUpWarning.classList.remove("hide");
+    popUpWarning.innerHTML = `<h2>${msg}</h2>`;
+    setTimeout(() => popUpWarning.classList.add("hide"), 2000);
 }
 
+//TODO: ???
+function inputSanitationandValidation() {
+
+}
 
 /*
 https://youtu.be/T1EJZsaqevU
@@ -128,5 +171,4 @@ https://www.youtube.com/watch?v=XJC5WB2Bwrc
 https://youtu.be/T1EJZsaqevUhttps://youtu.be/buvWBOZTfdchttps://www.youtube.com/watch?v=XJC5WB2Bwrc
 
 https://youtu.be/T1EJZsaqevU https://youtu.be/buvWBOZTfdc https://www.youtube.com/watch?v=XJC5WB2Bwrc
-
 */
